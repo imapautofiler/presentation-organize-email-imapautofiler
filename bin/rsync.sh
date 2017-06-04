93fb93b26649172ae1d64cd47570ece01e4100f2
Doug Hellmann <doug@doughellmann.com>
2017-06-04 18:56:39 -0400

    rename the presentation

--- bin/rsync.sh
+++ bin/rsync.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-FILES="*.html css js lib plugin *.svg img fonts"
+FILES="*.html css js lib plugin img fonts"
 
-ssh doughellmann.com 'mkdir -p ~/doughellmann.com/presentations/working-with-imap-email-servers'
+ssh doughellmann.com 'mkdir -p ~/doughellmann.com/presentations/organize-email-imapautofiler'
 
-rsync -av --progress $FILES doughellmann.com:~/doughellmann.com/presentations/working-with-imap-email-servers/
+rsync -av --progress $FILES doughellmann.com:~/doughellmann.com/presentations/organize-email-imapautofiler/
